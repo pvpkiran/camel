7e6e0f39bdb05e999c38afeee8108188e5a6427e
Panikiran <pvpkiran@gmail.com>
2018-08-23 13:48:16 +0200

    Added Rest configuration in camel

--- src/main/java/in/phani/springboot/camel/config/CamelConfig.java
+++ src/main/java/in/phani/springboot/camel/config/CamelConfig.java
@@ -4,6 +4,7 @@ import in.phani.springboot.camel.pojo.Product;
 import org.apache.camel.Exchange;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
+
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
@@ -20,7 +21,11 @@ public class CamelConfig {
 
                 from("direct:abc").to("bean:camelBeanComponent?method=processRoute");
 
-                restConfiguration().component("netty4-http").host("localhost").bindingMode(RestBindingMode.json).port(8070);
+                restConfiguration()
+                        .component("netty4-http")
+                        .host("localhost")
+                        /*.bindingMode(RestBindingMode.json)*/
+                        .port(8070);
                 rest()
                         .post("/addproduct")
                         .consumes("application/json")
